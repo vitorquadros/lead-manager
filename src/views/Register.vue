c2edb7e8be8a7e5fcaff859edfa05de6a4e950aa
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 11:50:31 -0300

    feat: create Register page

---
+++ src/views/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <Wrapper></Wrapper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Wrapper from '@/components/Utils/Wrapper.vue';
+
+export default defineComponent({
+  components: { Wrapper },
+});
+</script>
+
+<style scoped></style>
