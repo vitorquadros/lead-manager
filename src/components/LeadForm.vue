ad98b43309856fd8a9f6188a1932180cc93dff1d
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 12:05:34 -0300

    feat: create LeadForm component

---
+++ src/components/LeadForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="container">
+    <form></form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style scoped></style>
