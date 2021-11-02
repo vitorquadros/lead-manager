a9817d522698abb95c86d15d3b66f36acd6263ab
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 12:04:02 -0300

    feat: add BackLink component to NewLead page

--- src/views/NewLead.vue
+++ src/views/NewLead.vue
@@ -2,16 +2,20 @@
   <div>
     <Wrapper>
       <Header pageName="Novo Lead" />
+
+      <BackLink page="/">Voltar para leads</BackLink>
     </Wrapper>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Wrapper from '@/components/Utils/Wrapper.vue';
 import Header from '@/components/Header.vue';
+import BackLink from '@/components/BackLink.vue';
 
 export default defineComponent({
-  components: { Header },
+  components: { Header, Wrapper, BackLink },
 });
 </script>
 
