43398b8b65d4a640d7f8bfb4671fe517bcd07501
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-01 21:57:19 -0300

    feat: create Home page and import Header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <Wrapper>
+      <Header pageName="Painel de Leads" />
+    </Wrapper>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
+import Header from '@/components/Header.vue';
+import Wrapper from '@/components/Utils/Wrapper.vue';
 
 export default defineComponent({
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
+    Header,
+    Wrapper,
   },
 });
 </script>
