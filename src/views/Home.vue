17c50de5ab1f3ed4a57eac621eeb2a691a665b23
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 11:08:55 -0300

    feat: add register and new lead button on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div>
     <Wrapper>
       <Header pageName="Painel de Leads" />
+      <ButtonAdd page="register">Novo Usuário (+)</ButtonAdd>
+      <ButtonAdd page="new-lead">Novo Lead (+)</ButtonAdd>
     </Wrapper>
   </div>
 </template>
@@ -10,12 +12,14 @@
 import { defineComponent } from 'vue';
 import Header from '@/components/Header.vue';
 import Wrapper from '@/components/Utils/Wrapper.vue';
+import ButtonAdd from '@/components/Utils/ButtonAdd.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     Header,
     Wrapper,
+    ButtonAdd,
   },
 });
 </script>
