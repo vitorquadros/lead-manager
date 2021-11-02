343f99baba5681711e5d5d211324188b211d4387
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 11:58:07 -0300

    feat: add BackLink component to Register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,6 +3,8 @@
     <Title />
     <RegisterForm />
   </Wrapper>
+
+  <BackLink page="/">Voltar para leads</BackLink>
 </template>
 
 <script lang="ts">
@@ -10,9 +12,10 @@ import { defineComponent } from 'vue';
 import Wrapper from '@/components/Utils/Wrapper.vue';
 import Title from '@/components/Title.vue';
 import RegisterForm from '@/components/RegisterForm.vue';
+import BackLink from '@/components/BackLink.vue';
 
 export default defineComponent({
-  components: { Wrapper, Title, RegisterForm },
+  components: { Wrapper, Title, RegisterForm, BackLink },
 });
 </script>
 
