d6bd5c8fcfe2ab9dc9e4b119cf4e1324921183d3
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 11:20:30 -0300

    feat: create DragBlock component

---
+++ src/components/DragBlock.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="container">
+    <h3>{{ blockName }}</h3>
+    <draggable
+      v-model="list"
+      item-key="id"
+      @change="handleChange"
+      @add="handleChange"
+      @remove="handleChange"
+    >
+      <template #item="{ element }">
+        <Card id="card">
+          {{ element.name }}
+        </Card>
+      </template>
+    </draggable>
+
+    <!-- <div>
+      <pre>{{ list }}</pre>
+    </div> -->
+  </div>
+</template>
+
+<script lang="ts">
+import draggable from 'vuedraggable';
+import Card from '@/components/Card.vue';
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'DragBlock',
+  components: { draggable, Card },
+  props: ['blockName', 'groupConfig', 'finished'],
+});
+</script>
