a672ed81ef5129843f1c24e193661cf2ce3a139b
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-01 21:54:55 -0300

    feat: create Wrapper util component

---
+++ src/components/Utils/Wrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="container">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+#container {
+  background-color: white;
+  margin: 5rem auto;
+  max-width: 80rem;
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 5px 5px 5px gray;
+}
+</style>
