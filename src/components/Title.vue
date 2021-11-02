cd42a308c06d87d64a140bc5f41ce06d9a79f523
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-01 21:49:35 -0300

    feat: create Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p id="title">ELO<strong>GROUP</strong></p>
+  </div>
+</template>
+
+<style scoped>
+#title {
+  margin: 1.5rem 0 0 0;
+  padding: 10px 15px;
+  max-width: 300px;
+  text-align: center;
+  font-size: 4rem;
+  letter-spacing: 5px;
+  color: white;
+  background-color: #14222b;
+  border-radius: 0.3rem;
+}
+</style>
