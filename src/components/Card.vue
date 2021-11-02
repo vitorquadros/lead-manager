82f4274642812ac561058b8a110a58a64611d9b9
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 11:19:43 -0300

    feat: create Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Card',
+});
+</script>
+
+<style scoped>
+.card {
+  background: white;
+  padding: 1.4rem;
+  border-radius: 0.4rem;
+  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
+  font-size: 1.6rem;
+  margin-bottom: 0.8rem;
+  cursor: pointer;
+}
+</style>
