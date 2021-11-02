673cfb0d9e4d26fc8cdbe485e8b949aba6460555
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 14:47:32 -0300

    chore: update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,11 @@ module.exports = {
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
