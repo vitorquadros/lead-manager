c6ecef7f76fe6fe4f6995ba7b3d8af257c9b68e4
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-01 21:53:07 -0300

    feat: create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="container">
+    <header>
+      <Title />
+      <h3 v-if="pageName">{{ pageName }}</h3>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import Title from './Title.vue';
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+  components: { Title },
+  props: ['pageName'],
+});
+</script>
+
+<style scoped>
+#container {
+  background-color: white;
+}
+
+header {
+  display: flex;
+  margin: 2rem 2rem 0 2rem;
+  align-items: flex-end;
+  background-color: white;
+}
+
+header h3 {
+  font-size: 1.6rem;
+  margin: auto;
+}
+</style>
