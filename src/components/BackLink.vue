d2094b0bdf74726c18574a2f1e9b04955a517590
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 11:57:15 -0300

    feat: create BackLink component

---
+++ src/components/BackLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-link id="link" :to="page">
+      <slot />
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: ['page'],
+});
+</script>
+
+<style scoped>
+div {
+  display: flex;
+}
+
+#link {
+  font-size: 1.2rem;
+  margin: auto;
+  margin-bottom: 2rem;
+}
+</style>
