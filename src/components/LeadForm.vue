51af1b1a8a7c20c037c79b040a20f741e0f7d44b
Vitor Quadros <viitorquadros@hotmail.com>
2021-11-02 12:07:37 -0300

    feat: add info inputs on LeadForm component

--- src/components/LeadForm.vue
+++ src/components/LeadForm.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="container">
-    <form></form>
+    <form>
+      <div id="container-form">
+        <div id="form-info">
+          <div class="input-container">
+            <label>Nome *</label>
+            <input v-model="lead.name" type="text" />
+          </div>
+          <div class="input-container">
+            <label>Telefone *</label>
+            <input v-model="lead.tel" type="tel" />
+          </div>
+          <div class="input-container">
+            <label>Email *</label>
+            <input v-model="lead.email" type="email" />
+          </div>
+        </div>
+      </div>
+
+      <button @click="handleSubmit">Salvar</button>
+    </form>
   </div>
 </template>
 
